cca2269eba226a165a96a452fdf36d4e7f841384
ElvinMoisesSanchezM <elvinmoises.sanchez@gmail.com>
2020-11-01 08:26:22 -0600

    agregamos antd design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
+import es from '@angular/common/locales/es';
+
+registerLocaleData(es);
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { PagesHomeModule } from './Pages/home/home.module';
 import { PagesAdminModule } from './Pages/admin/admin.module';
+import { FormsModule } from '@angular/forms';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { es_ES } from 'ng-zorro-antd/i18n';
+
 
 @NgModule({
   declarations: [
@@ -17,9 +27,11 @@ import { PagesAdminModule } from './Pages/admin/admin.module';
     PagesAdminModule,
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: es_ES }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
