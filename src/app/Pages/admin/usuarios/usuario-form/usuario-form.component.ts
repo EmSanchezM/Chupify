6811f956eeeb7d94bbc36dbdc9698071b6e8e985
ElvinMoisesSanchezM <elvinmoises.sanchez@gmail.com>
2020-11-03 10:03:12 -0600

    validaciones de crud de usuarios

--- src/app/Pages/admin/usuarios/usuario-form/usuario-form.component.ts
+++ src/app/Pages/admin/usuarios/usuario-form/usuario-form.component.ts
@@ -52,12 +52,21 @@ export class UsuarioFormComponent implements OnInit {
       response=>{
         if(!response) this.router.navigateByUrl('admin/usuarios');
         this.usuarioEditar = response;
+
+        let index = 0;
+        for(let i=0; i<this.roles.length; i++){
+          
+          if(this.roles[i]['rolName'] == response.role['name']){
+            index = this.roles[i]['key'];
+          }
+        }
+        
         const usuarioForm = {
           first_name: response.first_name,
           last_name: response.last_name,
-          email: response.last_name,
+          email: response.email,
           password: response.password,
-          role: response.role
+          role: this.roles[index]['rolName']
         }
 
         this.formUsuario.setValue(usuarioForm);
@@ -70,7 +79,8 @@ export class UsuarioFormComponent implements OnInit {
       first_name: ['', [Validators.required, Validators.minLength(3)]],
       last_name: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.required, Validators.email]],
-      password:['',[Validators.required, Validators.minLength(4)]],
+      password:['',[Validators.required, Validators.minLength(6)]],
+      repeatpassword: ['', Validators.required],
       role: new FormControl('', Validators.required)
     })
   }
@@ -88,7 +98,7 @@ export class UsuarioFormComponent implements OnInit {
 
       if(params.id){
         let id = params.id;
-        //const usuario = new Usuario('',first_name, last_name, email, password, roleUser,'');
+        
         /*ACTUALIZAMOS USUARIO*/
         this.status = true;
         this.usuarioService.actualizarUsuario(usuario, id).subscribe(
@@ -101,7 +111,7 @@ export class UsuarioFormComponent implements OnInit {
             console.error(error);
           }
         )
-        this.router.navigateByUrl('usuarios');
+        this.router.navigateByUrl('admin/usuarios');
       }else{
         this.status = false;
         
@@ -113,9 +123,34 @@ export class UsuarioFormComponent implements OnInit {
           },
           error=>{ console.error('ERROR al agregar ', error)}
         )
-        this.router.navigateByUrl('usuarios');
+        this.router.navigateByUrl('admin/usuarios');
       }
     }
+  }
+
+  /*first_name, last_name, email, password, role */
+  //Validaciones
+  get nameNoValido() {
+    return this.formUsuario.get('first_name').invalid && this.formUsuario.get('first_name').touched
+  }
+
+  get lastNameNoValido() {
+    return this.formUsuario.get('last_name').invalid && this.formUsuario.get('last_name').touched
+  }
+  
+  get correoNoValido() {
+    return this.formUsuario.get('email').invalid && this.formUsuario.get('email').touched
+  }
+
+  get passwordNoValida() {
+    return this.formUsuario.get('password').invalid && this.formUsuario.get('password').touched
+  }
+
+  get repeatPasswordNoValida() {
+    const password = this.formUsuario.get('password').value;
+    const repeatpassword = this.formUsuario.get('repeatpassword').value;
+
+    return (password === repeatpassword) ? false : true;
   }
 
 }
